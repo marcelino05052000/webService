a99b305eb270e7517430d2250ef0ee4ddc1d2831
PRONATEC PRONATEC <pronatec@LABS.GARANHUNS.IFPE>
2018-07-04 09:49:35 -0300

    Algumas alterações necessárias

--- src/main/java/com/mycompany/projeto/Pessoa.java
+++ src/main/java/com/mycompany/projeto/Pessoa.java
@@ -5,13 +5,35 @@
  */
 package com.mycompany.projeto;
 
+import java.util.Objects;
+
 /**
  *
  * @author pronatec
  */
 public class Pessoa {
     String nome;
-    public Pessoa(String nome){
-        
+    int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
 }
--- src/main/java/com/mycompany/projeto/PessoaDAO.java
+++ src/main/java/com/mycompany/projeto/PessoaDAO.java
@@ -6,20 +6,47 @@
 package com.mycompany.projeto;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author pronatec
  */
 public class PessoaDAO {
-    
+
+    private static PessoaDAO instance;
+
     private ArrayList<Pessoa> pessoas;
-    
-    public PessoaDAO(){
+
+    private PessoaDAO() {
         this.pessoas = new ArrayList<Pessoa>();
     }
-    
-    public boolean adicionar(Pessoa pessoa){
-       return  this.pessoas.add(pessoa);
+
+    public static PessoaDAO getInstance() {
+        if (instance == null) {
+            instance = new PessoaDAO();
+        }
+        return instance;
+    }
+
+    public boolean adicionar(Pessoa pessoa) {
+        return this.pessoas.add(pessoa);
+    }
+
+    public boolean remover(Pessoa pessoa) {
+        return this.pessoas.remove(pessoa);
+    }
+
+    public Pessoa consultar(String codigo) {
+        for (Pessoa pessoa : this.pessoas) {
+            if (pessoa.getNome() == (codigo)) {
+                return pessoa;
+            }
+        }
+        return null;
+    }
+
+    public List<Pessoa> recuperar() {
+        return this.pessoas;
     }
 }
